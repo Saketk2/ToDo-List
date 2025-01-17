3187507564cde4d41b80a0ec9073ee7166257ac2
Saket Kolluru <saketkolluru.27@gmail.com>
2025-01-16 20:52:00 -0500

    Continued working on todo list

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlspatterns = [
-    path(' ', views.task_list, name = 'task_list')
+urlpatterns = [
+    path('', views.task_list, name='task_list'),
+    path('edit/<int:task_id>/', views.edit_task, name='edit_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task
+from .forms import TaskForm
 
 def task_list(request):
-    tasks = tasks.objects.all()
-    return render(request, 'task_list.html', {'tasks': tasks})
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks, 'form': form})
+
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id = task_id)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance = task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance = task)
+    tasks = Task.objects.all()
+    return render(request, 'edit_task.html', {'form': form, 'task': task})
